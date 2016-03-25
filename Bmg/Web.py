4d32f7a534b9e96eb3dd048e64fcb004059092c6
sehaure <seh@mirkosimage.eu>
2016-03-25 18:18:09 +0100

    web added

--- Bmg/Web.py
+++ Bmg/Web.py
@@ -1,5 +1,48 @@
+# -*- coding: utf-8 -*-
 import urllib
 import re
+
+
+class Bien(object):
+
+    def __init__(self, id):
+        self.id = id
+
+    def setInfos(self, url, price, surface, pieces, date, desc, image):
+        'set ratio in here'
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def date(self):
+        return self._date
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def image(self):
+        return self._image
+
+    @property
+    def pieces(self):
+        return self._pieces
+
+    @property
+    def surface(self):
+        return self._surface
+
+    @property
+    def desc(self):
+        return self._desc
+
+    @property
+    def ratio(self):
+        return self._ratio
+
 
 address = 'http://www.leboncoin.fr/ventes_immobilieres/offres/ile_de_france/seine_saint_denis/?th=1&ps=1&pe=4&ret=2'
 
@@ -7,11 +50,16 @@ sock = urllib.urlopen(address)
 html = sock.read()
 sock.close()
 
-reg = re.search(r'(studio)(.+)', html)
+# titles = re.findall(r'imgAlt="(.+);\s', html)
+prices = re.findall(r'item_price">(\d+ \d+)', html)
+rawLinks = re.findall(r'href="(.+)" title', html)
+ids = re.findall(r'ad_listid" : "(\d+)', html)
 
-print html
+infos = list()
+if prices:
+    for i in xrange(ids):
+        for link in rawLinks:
+            if ids[i] in link:
+                this = link
 
-if reg:
-	print reg.groups()
-else:
-	print 'found nothing'
+    print '{} Eurs {} {}'.format(prices[i], ids[i], this)
