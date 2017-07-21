a5a7fc02896a621ae610686a02f5ec3bc3fa380c
stuzzifr <stuzzifr@gmail.com>
2017-07-21 17:52:07 +0200

    new algo

--- api/rerout.py
+++ api/rerout.py
@@ -1,9 +1,9 @@
 from maya.OpenMaya import *
-
 from maya.OpenMayaUI import *
+
 import maya.cmds as cmds
-import pdb
 import math
+import pdb
 
 db = pdb.set_trace
 
@@ -12,21 +12,22 @@ class Point(object):
 
     def __init__(self, num):
 
-        self._iterV = MItMeshVertex()
+
+        self._fn = MItMeshVertex(Mesh.dag())
+
+        if num:
+            self.setTo(num)
 
         self._edgesAround = MIntArray()
         self._facesAround = MIntArray()
 
-        util = MScriptUtil()
-        prev = util.asIntPtr()
-        self.iterV.setIndex(num, prev)
-
-        self.index = num
-        self._pos = iterV.position(MSpace.kWorld)
+    @property
+    def fn(self):
+        return self._fn
 
     @property
     def pos(self):
-        return self._pos
+        return self.fn.position(MSpace.kWorld)
 
     @property
     def index(self):
@@ -38,22 +39,167 @@ class Point(object):
 
     @property
     def edgesAround(self):
-        iterV.getConnectedEdges(self._edgesAround)
-        return self._edgesAround
+        self.fn.getConnectedEdges(self._edgesAround)
+        return [self._edgesAround[i] for i in range(self._edgesAround.length())]
 
     @property
     def facesAround(self):
-        iterV.getConnectedFaces(self._facesAround)
-        return self._facesAround
+        self.fn.getConnectedFaces(self._facesAround)
+        return [self._facesAround[i] for i in range(self._facesAround.length())]
 
     @property
     def numEdges(self):
         util = MScriptUtil()
         num = util.asIntPtr()
 
-        self._iterV.numConnectedEdges(num)
+        self.fn.numConnectedEdges(num)
 
         return num
+
+    def setTo(self, num):
+
+        util = MScriptUtil()
+        prev = util.asIntPtr()
+
+        self.fn.setIndex(num, prev)
+        self.index = num
+
+    def getOppositeVtx(self, edgeNum):
+
+        util = MScriptUtil()
+        VtxPtr = util.asIntPtr()
+
+        self.fn.getOppositeVertex(VtxPtr, edgeNum)
+
+        return util.getInt(VtxPtr)
+
+
+class Edge(object):
+    def __init__(self, num=None):
+
+        self._fn = MItMeshEdge(Mesh.dag())
+        self._index = None
+
+        if num:
+            self.setTo(num)
+
+
+    @property
+    def fn(self):
+        return self._fn
+
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self, value):
+        self._index = value
+
+    @property
+    def aroundFaces(self):
+        connFaces = MIntArray()
+        self.fn.getConnectedFaces(connFaces)
+
+        return [connFaces[i] for i in range(connFaces.length())]
+
+    @property
+    def aroundEdges(self):
+        connEges = MIntArray()
+        self.fn.getConnectedEdges(connEges)
+
+        return [connEges[i] for i in range(connEges.length())]
+
+
+    @property
+    def pair(self):
+
+        first = self.fn.index(0)
+        second = self.fn.index(1)
+
+        return (first, second)
+
+    def setTo(self, num):
+
+        util = MScriptUtil()
+        prev = util.asIntPtr()
+
+        self.fn.setIndex(num, prev)
+        self.index = num
+
+
+class Face(object):
+
+    def __init__(self, num):
+
+        self._fn = MItMeshPolygon(Mesh.dag())
+        self._anchor = int()
+        self._index = None
+
+        if num:
+            self.fn.setTo(num)
+
+
+    @property
+    def nature(self):
+
+        nature = ''
+        num = self.fn.numConnectedEdges()
+
+        if num == 3:
+            return 'tris'
+
+        if num == 4:
+            return 'quad'
+
+        if num == 5:
+            return 'cinq'
+
+        return 'invalid'
+
+    @property
+    def fn(self):
+        return self._fn
+
+    @property
+    def anchor(self):
+        return self._anchor
+
+    @anchor.setter
+    def anchor(self, num):
+        self._anchor = num
+
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self, index):
+        self._index = index
+
+    @property
+    def vtx(self):
+
+        around = MIntArray()
+        self.fn.getVertices(around)
+
+        return [around[i] for i in range(around.length())]
+
+    @property
+    def edges(self):
+
+        around = MIntArray()
+        self.fn.getEdges(around)
+
+        return [around[i] for i in range(around.length())]
+
+    @property
+    def having(self, vertNum, edgeNum):
+        pass
+
+    @property
+    def twin(self):
+        pass
 
 
     def setTo(self, num):
@@ -61,17 +207,34 @@ class Point(object):
         util = MScriptUtil()
         prev = util.asIntPtr()
 
-        self._iterV.setIndex(num, prev)
-        self.index = self._iterV.index()
+        self.fn.setIndex(num, prev)
+        self.index = num
 
-    def getOppositeVtx(self, edgeNum):
+    @property
+    def leftVtx(self):
 
-        util = MScriptUtil()
-        VtxPtr = util.asIntPtr()
+        # -- vertice of the left, poly creation is anti CW
 
-        self._iterV.getOppositeVertex(VtxPtr, edgeNum)
+        leftVtx = -1
+        checkP = Point(self.anchor)
 
-        return util.getInt(VtxPtr)
+        if self.fn.nature == 'cinq':
+            for i in range(1, 3):
+                checkP.setTo(self.around[self.anchor - i])
+
+                if len(checkP.numEdges) == 4:
+                    leftVtx = checkP.index()
+                    break
+
+        if self.fn.nature == 'quad':
+            for i in range(1, 2):
+                checkP.setTo(self.around[self.anchor - i])
+
+                if len(checkP.numEdges) == 4:
+                    leftVtx = checkP.index()
+                    break
+
+        return leftVtx
 
 
 class Mesh(object):
@@ -137,105 +300,51 @@ class Mesh(object):
         self._way = value
 
 
-class Face(object):
-
-    def __init__(self):
-
-        self._iterF = MItMeshPolygon(Mesh.dag())
-        self._anchor = int()
-        self._index = int()
-
-    @property
-    def nature(self):
-
-        nature = ''
-        num = self._iterF.numConnectedEdges()
-
-        if num == 3:
-            return 'tris'
-
-        if num == 4:
-            return 'quad'
-
-        if num == 5:
-            return 'cinq'
-
-        return 'invalid'
-
-    @property
-    def anchor(self):
-        return self._anchor
-
-    @anchor.setter
-    def anchor(self, num):
-        self._anchor = num
-
-    @property
-    def index(self):
-        return self._index
-
-    @index.setter
-    def index(self, index):
-        self._index = index
-
-    @property
-    def around(self):
-
-        around = MIntArray()
-        self._iterF.getVertices(around)
-
-        return [around[i] for i in range(around.length())]
-
-    @property
-    def leftVtx(self):
-
-        # -- vertice of the left, poly creation is anti CW
-
-        leftVtx = int()
-        checkP = Point(self.anchor)
-
-        if self._iterF.nature == 'cinq':
-            for i in range(1, 3):
-                checkP.setTo(self.around[self.anchor - i])
-
-                if len(checkP.numEdges) == 4:
-                    leftVtx = checkP.index()
-                    break
-
-        if self._iterF.nature == 'quad':
-            for i in range(1, 2):
-                checkP.setTo(self.around[self.anchor - i])
-
-                if len(checkP.numEdges) == 4:
-                    leftVtx = checkP.index()
-                    break
-
-        return leftVtx
-
 class Rerout(object):
 
     def __init__(self):
 
         self.mesh = Mesh()
-        self.mesh.way = 1
-        self.analyse()
+        self.mesh.way = 1 # -- need to determine the direction
+        # self.analyse()
 
     def analyse(self):
 
         vert = self.mesh.startVert
-        for i in self.mesh.startVert.edgesAround:
+        # face = -- will need to find the good face
 
-            print '/n****loop', i
+        for f in vert.facesAround:
 
-            for e in range(0, 4):
+            print '/n****face', f
 
-                opposite = vert.getOppositeVtx(vert.edgesAround[self.mesh.way])
-                outString = ' v.{} -- edges:{} -- faces:{} -- way{}'
+            face = Face(f)
+            face.anchor = vert
 
-                print outString.format(vert.index,
-                                       vert.edgesAround,
-                                       vert.facesAround,
-                                       opposite
-                                       )
 
-                vert.setTo(opposite)
+
+            oppositeNum = vert.getOppositeVtx(vert.edgesAround[self.mesh.way])
+            opp = Point(oppositeNum)
+
+            outString = ' v.{} -- faces:{} -- v.{} -- face:{}'
+
+            print outString.format(vert.index,
+                                   vert.facesAround,
+                                   opp.index,
+                                   opp.facesAround
+                                   )
+
+            # face = reinit face
+
+# Anchor angle (consecutive edges on quad poly)
+# Anchor angle (if tris, consetcutive if biggest angle degree)
+# Anchor angle (if tris, next + 1 if not biggest angle degree)
+
+
+# Get first vert
+# Get faces around (faceA)
+
+# For each face
+    # check how much polys connected to the vert
+    # check nature of this face and get consecutive
+    # reset vert
+
